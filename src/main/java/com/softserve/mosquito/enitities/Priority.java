c685f273d78c2977664fb1b1f31f2b95f1d41948
Sergijv <Sergijv@gmail.com>
2018-05-07 11:19:10 +0300

    enum Priority + Status

--- src/main/java/com/softserve/mosquito/enitities/Priority.java
+++ src/main/java/com/softserve/mosquito/enitities/Priority.java
@@ -1,4 +1,11 @@
 package com.softserve.mosquito.enitities;
 
 public enum Priority {
+    LOW(1), MIDDLE(2), HIGH(3);
+
+    public final int priorityNumber;
+
+    Priority(int priorNumber) {
+        priorityNumber = priorNumber;
+    }
 }
