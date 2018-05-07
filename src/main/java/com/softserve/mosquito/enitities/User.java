1e764a8fa5ff218cf99e09c9378b9c4f577ac7a7
Oleh Onufryk <olehonufryk@gmail.com>
2018-05-07 11:19:46 +0300

    User.java has been created

--- src/main/java/com/softserve/mosquito/enitities/User.java
+++ src/main/java/com/softserve/mosquito/enitities/User.java
@@ -1,4 +1,100 @@
 package com.softserve.mosquito.enitities;
 
+import java.util.Objects;
+import java.util.Set;
+
 public class User {
+
+    private Long id;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    Set<Specialization> specializations;
+
+    public User() {
+    }
+
+    public User(String email, String firstName, String lastName, String password, Set<Specialization> specializations) {
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.specializations = specializations;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Specialization> getSpecializations() {
+        return specializations;
+    }
+
+    public void setSpecializations(Set<Specialization> specializations) {
+        this.specializations = specializations;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(password, user.password) &&
+                Objects.equals(specializations, user.specializations);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(id, email, firstName, lastName, password, specializations);
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", specializations=" + specializations +
+                '}';
+    }
 }
