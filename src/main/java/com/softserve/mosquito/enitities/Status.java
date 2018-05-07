534b0b75516f74a969d8a551c36d8e2214e8a3fb
Petro Sementsiv <forester4455@gmail.com>
2018-05-07 22:04:38 +0300

    Changed enums Priority and Status, added default values of priority and status in Task class.

--- src/main/java/com/softserve/mosquito/enitities/Status.java
+++ src/main/java/com/softserve/mosquito/enitities/Status.java
@@ -1,14 +1,16 @@
 package com.softserve.mosquito.enitities;
 
 public enum Status {
-    READY(1), IN_PROGRESS (2), DONE(3);
+    READY(1L), IN_PROGRESS(2L), DONE(3L);
 
 
-    public final int statusNumber;
+    private final Long statusNumber;
 
-    Status(int statNumber) {
+    Status(Long statNumber) {
         statusNumber = statNumber;
     }
 
-
+    public Long getStatusNumber() {
+        return statusNumber;
+    }
 }
