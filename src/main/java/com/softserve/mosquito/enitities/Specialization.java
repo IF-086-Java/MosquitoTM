08bc42f7583edde175d86ab0535399eaf1ac9fd6
Barta Volodymyr <bartavol@gmail.com>
2018-05-08 07:25:57 +0300

    Initial enum Specialization

--- src/main/java/com/softserve/mosquito/enitities/Specialization.java
+++ src/main/java/com/softserve/mosquito/enitities/Specialization.java
@@ -1,4 +1,15 @@
 package com.softserve.mosquito.enitities;
 
 public enum Specialization {
+	DEVELOPER(1L), ARCHITECT(2L), PROJECT_MANAGER(3L), TEST(4L);
+	
+	private Long specializationNumber;
+	
+	Specialization(Long specializationNumber){
+		this.specializationNumber = specializationNumber;
+	}
+	
+	public Long getSpecializationNumber() {
+		return this.specializationNumber;
+	}
 }
