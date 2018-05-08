c8cbac5df59c84456fa574a9ac425d229eaba680
Serhiy Khalak <serghalak@gmail.com>
2018-05-08 11:21:54 +0300

    new entities Estimation LogWork

--- src/main/java/com/softserve/mosquito/enitities/Estimation.java
+++ src/main/java/com/softserve/mosquito/enitities/Estimation.java
@@ -1,4 +1,70 @@
 package com.softserve.mosquito.enitities;
 
+import java.util.List;
+
 public class Estimation {
+
+    private Long id;
+    private int estimation;
+    private int remaining;
+    private List<LogWork> logWorks;
+
+
+    public Estimation(Long id, int estimation,
+                      int remaining, List<LogWork> logWorks) {
+        this.id = id;
+        this.estimation = estimation;
+        this.remaining = remaining;
+        this.logWorks = logWorks;
+    }
+
+    public Estimation(Long id) {
+        this.id = id;
+    }
+
+    public Estimation() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getEstimation() {
+        return estimation;
+    }
+
+    public void setEstimation(int estimation) {
+        this.estimation = estimation;
+    }
+
+    public int getRemaining() {
+        return remaining;
+    }
+
+    public void setRemaining(int remaining) {
+        this.remaining = remaining;
+    }
+
+    public List<LogWork> getLogWorks() {
+        return logWorks;
+    }
+
+    public void setLogWorks(List<LogWork> logWorks) {
+        this.logWorks = logWorks;
+    }
+
+    @Override
+    public String toString() {
+        return "Estimation{" +
+                "id=" + id +
+                ", estimation=" + estimation +
+                ", remaining=" + remaining +
+                ", logWorks=" + logWorks +
+                '}';
+    }
+
 }
--- src/main/java/com/softserve/mosquito/enitities/LogWork.java
+++ src/main/java/com/softserve/mosquito/enitities/LogWork.java
@@ -1,4 +1,96 @@
 package com.softserve.mosquito.enitities;
 
+import java.util.Date;
+
 public class LogWork {
+
+    private Long id;
+    private String logDescription;
+    private Date createdDate;
+    private Long userId;
+    private int commitLog;
+    private Long estimationId;
+
+
+    public LogWork(Long id, String logDescription,
+                   Date createdDate, Long userId,
+                   int commitLog, Long estimationId) {
+        this.id = id;
+        this.logDescription = logDescription;
+        this.createdDate = createdDate;
+        this.userId = userId;
+        this.commitLog = commitLog;
+        this.estimationId = estimationId;
+
+    }
+
+    public LogWork(Long id, Long userId, Long estimationId) {
+        this.id = id;
+        this.userId = userId;
+        this.estimationId = estimationId;
+    }
+
+    public LogWork() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getLogDescription() {
+        return logDescription;
+    }
+
+    public void setLogDescription(String logDescription) {
+        this.logDescription = logDescription;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Long userId) {
+        this.userId = userId;
+    }
+
+    public int getCommitLog() {
+        return commitLog;
+    }
+
+    public void setCommitLog(int commitLog) {
+        this.commitLog = commitLog;
+    }
+
+    public Long getEstimationId() {
+        return estimationId;
+    }
+
+    public void setEstimationId(Long estimationId) {
+        this.estimationId = estimationId;
+    }
+
+    @Override
+    public String toString() {
+        return "LogWork{" +
+                "id=" + id +
+                ", logDescription='" + logDescription + '\'' +
+                ", createdDate=" + createdDate +
+                ", userId=" + userId +
+                ", commitLog=" + commitLog +
+                ", estimationId=" + estimationId +
+                '}';
+    }
+
 }
