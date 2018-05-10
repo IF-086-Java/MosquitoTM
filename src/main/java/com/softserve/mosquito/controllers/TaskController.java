266fc5d80370db0067dc4ca3421a70ec58b331b0
Oleh Onufryk <olehonufryk@gmail.com>
2018-05-10 23:30:08 +0300

    Edit method for REST query in TaskController and LogWorkController

--- src/main/java/com/softserve/mosquito/controllers/TaskController.java
+++ src/main/java/com/softserve/mosquito/controllers/TaskController.java
@@ -24,49 +24,71 @@ public class TaskController {
     ServletContext context;
 
     @POST
-    @Consumes("text/plain")
-    public String createSubTaskOrProject(@QueryParam("parentId") Long parentId) {
-        return "Created task with ID " + parentId;
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String createSubTaskOrProject(@QueryParam("parentId") Long parentId,
+                                         @FormParam("task_name") String taskName,
+                                         @FormParam("priority") String priority,
+                                         @FormParam("specialization") String specialization,
+                                         @FormParam("assignee") String assignee,
+                                         @FormParam("estimation") String estimation) {
+        return "Created task with " + parentId + " " + taskName
+                + " " + priority + " " + specialization + " " + assignee
+                + " " + estimation;
     }
 
     @GET
-    @Path("/user-task")
-    public String getUserTasks(@QueryParam("workerId") Long workerId) {
+    @Path("/worker-id")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getWorkerTasks(@QueryParam("workerId") Long workerId) {
         return "User tasks " + workerId;
     }
 
     @GET
-    @Path("/owner-task")
+    @Path("/owner-id")
+    @Produces(MediaType.TEXT_PLAIN)
     public String getOwnerTasks(@QueryParam("ownerId") Long ownerId) {
         return "Owner id " + ownerId;
     }
 
     @GET
     @Path("{taskId}")
+    @Produces(MediaType.TEXT_PLAIN)
     public String getTaskById(@PathParam("taskId") Long taskId) {
         return "Got task with id " + taskId;
     }
 
     @PUT
     @Path("/{taskId}")
-    public String updateTask(@PathParam("taskId") Long taskId) {
-        return "Task updated " + taskId;
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String updateTask(@PathParam("taskId") Long taskId,
+                             @FormParam("task_name") String taskName,
+                             @FormParam("priority") String priority,
+                             @FormParam("specialization") String specialization,
+                             @FormParam("assignee") String assignee,
+                             @FormParam("estimation") String estimation,
+                             @FormParam("status") String status) {
+        return "Task updated " + taskId + " " + taskName
+                + " " + priority + " " + specialization + " " + assignee
+                + " " + estimation + " " + status;
     }
 
     @GET
+    @Produces(MediaType.TEXT_PLAIN)
     public String getSubTaskOrProject(@QueryParam("parentId") Long parentId) {
         return "Got subTask/project for ID " + parentId;
     }
 
     @DELETE
     @Path("/{taskId}")
+    @Consumes(MediaType.APPLICATION_JSON)
     public String deleteTask(@PathParam("taskId") Long taskId) {
         return "Deleted task with id " + taskId;
     }
 
     @GET
     @Path("/status")
-    public String getTaskByStatus(@QueryParam("taskStatus")Long statusId){
+    @Produces(MediaType.TEXT_PLAIN)
+    public String getTaskByStatus(@QueryParam("taskStatus") Long statusId) {
         return "Got task with status id " + statusId;
     }
 
