4d0063d31845b360c6c14616c6bd9dd0158d3f14
Barta Volodymyr <bartavol@gmail.com>
2018-05-18 06:53:52 +0300

    Modified Login

--- src/main/java/com/softserve/mosquito/controllers/IndexController.java
+++ src/main/java/com/softserve/mosquito/controllers/IndexController.java
@@ -1,22 +1,26 @@
 package com.softserve.mosquito.controllers;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import com.softserve.mosquito.dtos.UserLoginDto;
 import com.softserve.mosquito.dtos.UserRegistrationDto;
 import com.softserve.mosquito.enitities.User;
+import com.softserve.mosquito.services.UserService;
 import com.softserve.mosquito.validation.UserValidation;
 
 @Path("/")
 public class IndexController {
-
-    private UserValidation validation = new UserValidation();
+    private UserService userService = new UserService();
+    private UserValidation validation = new UserValidation(new UserService());
 
     @GET
     @Produces(MediaType.TEXT_HTML)
@@ -27,16 +31,21 @@ public class IndexController {
     }
     
     /**
-     * @param user : JSON body from Front-end in POST
-     * @return JSON to Front 
+     * 
      * */
     @POST
     @Path("/login")
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public User login(UserLoginDto user) {
-       if(validation.isLoginValid(user)) {
-    	   return new User(user.getEmail(), "", "", user.getPassword());
+    public User login(UserLoginDto userLoginDto, @Context HttpServletRequest request) {
+       if(validation.isLoginValid(userLoginDto)) {
+    	   User user = userService.getUserByEmail(userLoginDto.getEmail());
+    	   
+    	   HttpSession session = request.getSession();
+    	   session.setAttribute("user_id", user.getId());
+    	   session.setAttribute("name", user.getFirstName() + " " + user.getLastName());
+    	   
+    	   return user;
        }else {
     	   return null;
        }
--- src/main/java/com/softserve/mosquito/validation/UserValidation.java
+++ src/main/java/com/softserve/mosquito/validation/UserValidation.java
@@ -6,15 +6,23 @@ import com.softserve.mosquito.enitities.User;
 import com.softserve.mosquito.services.UserService;
 
 public class UserValidation {
-	private UserService userService = new UserService();
+	private UserService userService;
+	private static final int MIN_EMAIL_LENGTH = 6;
+	private static final int MAX_EMAIL_LENGTH = 30;
+	private static final int MIN_PASSWORD_LENGTH = 5;
+	private static final int MAX_PASSWORD_LENGTH = 30;
 
-    public boolean isLoginValid(UserLoginDto userLoginDto){
+    public UserValidation(UserService userService) {
+		this.userService = userService;
+	}
+
+	public boolean isLoginValid(UserLoginDto userLoginDto){
         if (userLoginDto != null) { 
         	String userLoginEmail = userLoginDto.getEmail();
         	String userLoginPassword = userLoginDto.getPassword();
         	
-        	if(userLoginEmail != null && userLoginEmail.length() > 10 && userLoginEmail.length() < 35 
-        			&& userLoginPassword != null && userLoginPassword.length() > 5 && userLoginPassword.length() < 35) {
+        	if(userLoginEmail != null && userLoginEmail.length() >= MIN_EMAIL_LENGTH && userLoginEmail.length() <= MAX_EMAIL_LENGTH 
+        			&& userLoginPassword != null && userLoginPassword.length() >= MIN_PASSWORD_LENGTH && userLoginPassword.length() <= MAX_PASSWORD_LENGTH) {
         		User user = userService.getUserByEmail(userLoginDto.getEmail());
         		        		
         		if(user != null) {
